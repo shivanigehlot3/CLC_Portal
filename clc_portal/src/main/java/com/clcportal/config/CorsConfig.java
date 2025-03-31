8be945d094badb6a90c9fcfff557a53f3fe81d9d
shivanigehlot3 <144298742+shivanigehlot3@users.noreply.github.com>
2025-04-01 01:04:47 +0530

    Update CorsConfig.java

--- clc_portal/src/main/java/com/clcportal/config/CorsConfig.java
+++ clc_portal/src/main/java/com/clcportal/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5503")
+                        .allowedOrigins("http://127.0.0.1:5500")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
